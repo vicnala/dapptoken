87a6a0a346a2ca466615269cd687ce357b670350
Víctor Navarro <vicnala@gmail.com>
2019-07-02 08:55:51 +0200

    Updates

---
+++ precache-manifest.00b4ade0b4de2778824eabb3cb9a01b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5edecec9fb2a614c5e96598c94f69bd",
+    "url": "/dapptoken/index.html"
+  },
+  {
+    "revision": "68f904ce6ea81c741e69",
+    "url": "/dapptoken/static/css/2.545c6387.chunk.css"
+  },
+  {
+    "revision": "6858946b7b337271d838",
+    "url": "/dapptoken/static/css/main.11c7cd58.chunk.css"
+  },
+  {
+    "revision": "68f904ce6ea81c741e69",
+    "url": "/dapptoken/static/js/2.a9a432e2.chunk.js"
+  },
+  {
+    "revision": "6858946b7b337271d838",
+    "url": "/dapptoken/static/js/main.0df8f16a.chunk.js"
+  },
+  {
+    "revision": "27b2b399fd5fb99c118a",
+    "url": "/dapptoken/static/js/runtime~main.a719e265.js"
+  }
+]);
