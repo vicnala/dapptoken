9d0d0bc0c096179dbac24266e96ac4fd5f891521
Víctor Navarro <vicnala@gmail.com>
2019-07-05 07:50:57 +0200

    Using useCacheSend the right way

--- app/src/components/Transfer.js
+++ app/src/components/Transfer.js
@@ -1,30 +1,21 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import { Card, Form, Button } from 'react-bootstrap';
 import { drizzleReactHooks } from 'drizzle-react';
+
+
+
 
 export default () => {
   const [address, setAddress] = useState('');
   const [amount, setAmount] = useState('');
-  const [submit, setSubmit] = useState(false);
 
   // https://github.com/trufflesuite/drizzle-react#usecachesend
   const { useCacheSend } = drizzleReactHooks.useDrizzle();
   // https://github.com/trufflesuite/drizzle-react/blob/04bdb7883271dd300eff3aab47a132869f02a627/src/hooks/create-use-cache-send.js#L18
   // the function returns status, not transactions
   const { send, status } = useCacheSend('Token', 'transfer');
-  const transferTokens = useCallback(({address, amount}) => send(address, amount), [send]);
-
-
   const { drizzle } = drizzleReactHooks.useDrizzle();
   const toWei = drizzle.web3.utils.toWei;
-
-  useEffect(() => {
-    if (submit) {
-      setSubmit(false);
-      // convert to wei
-      transferTokens({address, amount: toWei(`${amount || 0}`)});
-    }
-  }, [submit, transferTokens, address, amount, toWei]);
 
   return (
     <Card bg="dark" text="white">
@@ -48,7 +39,9 @@ export default () => {
               onInput={e => setAmount(e.target.value)}
             />
           </Form.Group>
-          <Button variant="secondary" size="lg" onClick={(e) => setSubmit(true)}>
+          <Button variant="secondary" size="lg"
+            onClick={useCallback(() => send(address, toWei(amount.toString())), [send, address, amount, toWei])}
+          >
             { (!status || status === "success") ? "TRANSFER" : status.toUpperCase() }
           </Button>
         </Form>
